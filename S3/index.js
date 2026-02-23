a7f84b5944ad5d0461249268a87b0565555c10e9
Ayush0115D <dhakreayush578@gmail.com>
2026-02-23 22:57:34 +0530

    presigned urls for putobjects

--- S3/index.js
+++ S3/index.js
@@ -1,7 +1,6 @@
-
 import dotenv from "dotenv";
 dotenv.config();
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand,PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const s3Client = new S3Client({
   region: "ap-south-1",
@@ -19,10 +18,24 @@ async function getObjectURL(key) {
   const url = await getSignedUrl(s3Client, command);
   return url;
 }
+async function putObject(filename,contentType) {
+  const command = new PutObjectCommand({
+    Bucket: "ayushd-private",
+    Key: `uploads/user-uploads/${filename}`,
+    ContentType:  "video/mp4",
+  });
+  const url=await getSignedUrl(s3Client, command);
+  return url;
+}
 
 async function init() {
   const url = await getObjectURL("super-saiyan-blue-3840x2160-17603.jpg");
   console.log("URL:", url);
+  console.log("URL for uploading:", await putObject(`video-${Date.now()}.mp4`,"video/mp4")
+);
+ const url2 = await getObjectURL("uploads/user-uploads/image-1771859654600.jpg");
+  console.log("URL2:", url2);
+  const url3 = await getObjectURL("uploads/user-uploads/video-1771866048285.mp4");
+  console.log("URL3:", url3);
 }
-
 init();
